baa9c1a91cece41158443318aef02fd41a2ca2ed
INTELIGENCIA <darodriguesufc@gmail.com>
2024-11-29 16:25:13 -0300

    OI

--- project/src/main/java/org/example/Controller.java
+++ project/src/main/java/org/example/Controller.java
@@ -3,6 +3,7 @@ package org.example;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +19,14 @@ import java.net.UnknownHostException;
 @RequestMapping("/look_ip")
 public class Controller {
     @GetMapping
-    public ResponseEntity getLocation(@RequestParam String ip) throws IOException, GeoIp2Exception {
+    public ResponseEntity getLocation(@RequestParam String ip, @RequestParam Boolean from_me, HttpServletRequest request) throws IOException, GeoIp2Exception {
+        String used_ip = ip;
+        if(from_me){//o navegador envia automaticamente
+            used_ip = request.getHeader("X-FORWARDED-FOR");
+        }
         File database = new File(Main.class.getClassLoader().getResource("GeoLite2-City.mmdb").getFile());
         DatabaseReader reader = new DatabaseReader.Builder(database).build();
-        InetAddress ipAddress = InetAddress.getByName(ip);//ipv4 e ipv6
+        InetAddress ipAddress = InetAddress.getByName(used_ip);//ipv4 e ipv6
         CityResponse response = reader.city(ipAddress);
         System.out.println(response);
         System.out.println(response.getCity().getName());
